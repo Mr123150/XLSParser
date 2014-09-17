d7ef64aeec41f36f8b417fbe7010175a72f9f444
Viktor Snesarevskiy <snesarevskiy@yandex.ru>
2014-09-17 07:18:08 +0400

    Changed excel to poi (error at start)

--- src/ru/mr123150/xlsparser/MainActivity.java
+++ src/ru/mr123150/xlsparser/MainActivity.java
@@ -1,19 +1,20 @@
 package ru.mr123150.xlsparser;
 
-import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
-import jxl.Cell;
-import jxl.Sheet;
-import jxl.Workbook;
-import jxl.WorkbookSettings;
-import jxl.read.biff.BiffException;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.widget.EditText;
 
@@ -31,7 +32,7 @@ public class MainActivity extends Activity {
 		tv=(EditText)findViewById(R.id.tv);
 		
 		try {
-			List<String> res = read("");
+			List<String> res = read("/sdcard/download/kib1.xls");
 			String[] strres = res.toArray(new String[0]);
 			tv.setText(Arrays.toString(strres));
 		} catch (IOException e) {
@@ -47,15 +48,15 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
-	public List<String> read(String key) throws IOException  {
+	public List<String> read(String fileName) throws IOException  {
 	    List<String> resultSet = new ArrayList<String>();
 
 	    //File inputWorkbook = new File(inputFile);
-	    File inputWorkbook = new File("/sdcard/download/kib1.xls");
+	    /*File inputWorkbook = new File("/sdcard/download/kib1.xls");
 	    if(inputWorkbook.exists()){
 	        Workbook w;
 	        WorkbookSettings ws = new WorkbookSettings();
-	        ws.setEncoding("utf-16le");
+	        ws.setEncoding("UTF-16BE");
 	        try {
 	            w = Workbook.getWorkbook(inputWorkbook,ws);
 	            // Get the first sheet
@@ -84,7 +85,47 @@ public class MainActivity extends Activity {
 	    if(resultSet.size()==0){
 	        resultSet.add("Data not found..!");
 	    }
-	    return resultSet;
+	    return resultSet;*/
+	    FileInputStream fis = new FileInputStream(fileName);
+        
+        //Create Workbook instance for xlsx/xls file input stream
+	    Workbook workbook = new HSSFWorkbook(fis);
+        /*if(fileName.toLowerCase().endsWith("xlsx")){
+            workbook = new XSSFWorkbook(fis);
+        }else if(fileName.toLowerCase().endsWith("xls")){
+            workbook = new HSSFWorkbook(fis);
+        }*/
+        
+      //Get the nth sheet from the workbook
+	    Sheet sheet = workbook.getSheetAt(0);
+         
+        //every sheet has rows, iterate over them
+        Iterator<Row> rowIterator = sheet.iterator();
+        while (rowIterator.hasNext())
+        {
+             
+            //Get the row object
+        	Row row = rowIterator.next();
+             
+            //Every row has columns, get the column iterator and iterate over them
+            Iterator<Cell> cellIterator = row.cellIterator();
+              
+            while (cellIterator.hasNext())
+            {
+                //Get the Cell object
+            	Cell cell = cellIterator.next();
+                resultSet.add(cell.getStringCellValue().trim());
+            } //end of cell iterator
+            //Country c = new Country(name, shortCode);
+            
+            
+        } //end of rows iterator
+            
+         
+        //close file input stream
+        fis.close();
+        
+        return resultSet;
 	}
 
 }
